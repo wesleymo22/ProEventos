4d44e0b73126421fa4846381cd0b7faa166c6578
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-12-06 10:48:31 -0300

    Adicionando HttpGet e Cors

--- Back/src/ProEventos.API/Startup.cs
+++ Back/src/ProEventos.API/Startup.cs
@@ -29,10 +29,12 @@ namespace ProEventos.API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            
             services.AddDbContext<DataContext>(
                 context => context.UseSqlServer(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -54,6 +56,10 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(cors => cors.AllowAnyHeader()
+                                    .AllowAnyMethod()
+                                    .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any ;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): any {
+    this.http.get('https://localhost:7024/api/eventos/').subscribe(
+      respose => this.eventos = respose,
+      error => console.log(error)
+    );
+  }
+
+}
