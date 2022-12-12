8146eb889a5146dde9771587b4b17be281ec2a54
Wesley Moraes Franco <90381944+wesleymo22@users.noreply.github.com>
2022-12-12 09:45:47 -0300

    Entidades de Dominio (Erro na API)

--- Back/src/ProEventos.Domain/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace ProEventos.Domain;
-public class Class1
-{
-
-}
---
+++ Back/src/ProEventos.Domain/Palestrante.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Domain
+{
+    public class Palestrante
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string MiniCurriculo { get; set; }
+        public string ImagemUrl { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<RedeSocial> RedesSociais { get; set; }
+        public IEnumerable<PalestranteEvento> PalestranteEvento { get; set; }
+
+
+    }
+}
